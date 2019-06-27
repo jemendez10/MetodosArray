9cf34162e06e15be6b7582d89138746f03a6461f
jemendez10 <jemendez10@misena.edu.co>
2019-06-27 09:01:30 -0500

    EjercioArray1

---
+++ metodos.js
@@ -0,0 +1,19 @@
+var txt1="APRENDIZ"
+// var arr=txt1.split("")
+var txt2=txt1
+document.getElementById('ejercicioArray1').innerHTML=txt1.toLowerCase()+" sena"
+document.getElementById('ejercicioArray2').innerHTML=txt1.split("").join("+")
+ document.getElementById('ejercicioArray3').innerHTML=txt1.split("").reverse().join("")
+// var array1=[
+// "Bateria",
+// "Bajo",
+// "Guitarra",
+// "saxofon",
+// "Violin"
+// ]
+// // var textoArray=array1.toString()
+// // var textoarray2=array1.join(" [^_^] ")
+// // array1.pop()
+// document.getElementById('ejercicioArray').innerHTML=array1.push("Flauta")
+// // document.write(array1.toString())
+// console.log(array1)
